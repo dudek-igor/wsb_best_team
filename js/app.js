8f8e5b783bb3f6b4e47dcfaec90ae409979537fb
CarolKuch <kar.kucharska@gmail.com>
2021-02-09 23:24:27 +0100

    Code refactoring

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 $(function () {
-    let request = new XMLHttpRequest();
-    var data;
-    var day;
-    var hour;
+    let data;
+    let day;
+    let hour;
+    let xhr = new XMLHttpRequest();
 
-    request.open('GET', 'https://api.apify.com/v2/key-value-stores/3Po6TV7wTht4vIEid/records/LATEST?disableRedirect=true/runs?token=Re5MdToMojdDhxuhkBSNrjCWw', true);
-    request.onload = function () {
-        data = JSON.parse(this.response);
+    xhr.open('GET', 'https://api.apify.com/v2/key-value-stores/3Po6TV7wTht4vIEid/records/LATEST?disableRedirect=true/runs?token=Re5MdToMojdDhxuhkBSNrjCWw', true);
+    xhr.onload = function () {
+        data = JSON.parse(xhr.response);
         document.getElementById("infections-counter").innerHTML = data.infected;
         document.getElementById("recovered-counter").innerHTML = data.recovered;
         document.getElementById("deceased-counter").innerHTML = data.deceased;
@@ -14,7 +14,7 @@ $(function () {
         day = lastUpdateTime[0];
         hour = lastUpdateTime[1].slice(0,5);
         document.getElementById("last-update").innerHTML = "Ostatnia aktualizacja danych nastąpiła w dniu " + day + " o godzinie " + hour;
-    }
-    request.send();
+    }    
+    xhr.send();
 });
 
